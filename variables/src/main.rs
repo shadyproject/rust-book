8eab30545470e8b79a854ec69a851edeba03c3a7
Christopher Martin <cmartin@twilio.com>
2020-02-27 00:21:51 -0700

    Numeric operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -17,9 +17,31 @@ fn main() {
     println!("The value of x is {}", x);
      */
 
+    /*
     let x = 2.1; //f64 by default
     let y: f32 = 3.2;
 
     println!("The value of x is {}", x);
     println!("The value of y is {}", y);
+     */
+
+    //addition
+    let x = 5 + 10;
+    println!("The value of x is {}", x);
+
+    //subtraction
+    let x = 10 - 5;
+    println!("The value of x is {}", x);
+
+    //multiplication
+    let x = 4 * 30;
+    println!("The value of x is {}", x);
+
+    //division
+    let x = 10/2;
+    println!("The value of x is {}", x);
+
+    //remainder
+    let x = 43 % 5;
+    println!("The value of x is {}", x);
 }
