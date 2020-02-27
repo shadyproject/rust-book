e78bca6cb642265b1fa76aaa0dfa90c544eef09d
Christopher Martin <cmartin@twilio.com>
2020-02-27 00:10:27 -0700

    gst

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,10 +8,18 @@ fn main() {
     println!("THe value of x is {}", x);
      */
 
+    /*
     //variable shadowing
     let x = 5;
     let x = x + 1;
     let x = x * 2;
 
     println!("The value of x is {}", x);
+     */
+
+    let x = 2.1; //f64 by default
+    let y: f32 = 3.2;
+
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
 }
