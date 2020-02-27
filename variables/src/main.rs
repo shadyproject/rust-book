2dff62f8cd1860094c9e0a485fd74a36f9db987e
Christopher Martin <cmartin@twilio.com>
2020-02-26 23:57:46 -0700

    variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
+    /*
+    //mutable variable
     let mut x = 5;
     println!("The value of x is {}", x);
 
     x = 6;
     println!("THe value of x is {}", x);
+     */
+
+    //variable shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    println!("The value of x is {}", x);
 }
