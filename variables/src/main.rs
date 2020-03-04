21b1ea2968be8357ee2a26d4964829fefab3d452
Christopher S Martin <shady@shadyproject.net>
2020-03-03 22:34:53 -0500

    more tuples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -69,4 +69,11 @@ fn main() {
     println!("The value of x is {}", x);
     println!("The value of y is {}", y);
     println!("The value of z is {}", z);
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    println!("The value of five_hundred is {}", five_hundred);
+    println!("The value of six_point_four is {}", six_point_four);
+    println!("The value of one is {}", one);
 }
