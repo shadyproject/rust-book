7150793d430bf14537ed3a68cbdb044ae9f1c8a9
Christopher S Martin <shady@shadyproject.net>
2021-01-07 22:20:33 -0800

    add variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
+    const MAX_POINTS: u32 = 100_00;
+    println!("Total number of points available: {}", MAX_POINTS);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is: {}", x);
 }
