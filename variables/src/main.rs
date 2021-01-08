7499248c0a3b38df735c3b518db12556cfa6386a
Christopher S Martin <shady@shadyproject.net>
2021-01-07 22:23:52 -0800

    shadowing vs mut

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     const MAX_POINTS: u32 = 100_00;
     println!("Total number of points available: {}", MAX_POINTS);
 
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is: {}", x);
+    // shadowing lets you change the type of the variable, unlike mut
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("There are {} spaces.", spaces);
 }
