aa273a3bb8a1f19bf9bbba54a3262f142b2e28b7
Christopher S Martin <shady@shadyproject.net>
2021-01-21 16:57:54 -0800

    space issues

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,3 @@ fn main() {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     println!("{}", months[11]);
 }
- 
