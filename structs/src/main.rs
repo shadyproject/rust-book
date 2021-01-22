62d8390521f1a1cda5621bda8d82eb80722745fa
Christopher S Martin <shady@shadyproject.net>
2021-01-21 23:51:09 -0800

    struct update syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -9,6 +9,15 @@ fn main() {
     let user1 = build_user(String::from("test@test.com"), String::from("test_user"));
 
     println!("{}", user1.username);
+
+    // use the struct update syntax to use values from existing struct
+    let user2 = User {
+        email: String::from("another@test.com"),
+        username: String::from("anoter_user"),
+        ..user1
+    };
+
+    println!("{} ({})", user2.username, user2.email);
 }
 
 fn build_user(email: String, username: String) -> User {
