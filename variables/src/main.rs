f71fec639dc7615ce49088d332d3d4f387b8e613
Christopher S Martin <shady@shadyproject.net>
2021-01-21 16:57:08 -0800

    more arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
     println!("{}", a[0]);
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    println!("{}", months[11]);
 }
+ 
