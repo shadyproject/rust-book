3e2c131da713728ca525c68673cf1ccb608bbe8a
Christopher S Martin <shady@shadyproject.net>
2021-01-21 16:48:22 -0800

    direct tuple access

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     //println!("Tuple: {}", tup); //doens't work
 
+    // destructuring
     let (e, y, z) = tup;
     println!("The value of y is: {}", y);
+
+    // direct access
+    let five_hundred = tup.0;
+    println!("{}", five_hundred);
 }
