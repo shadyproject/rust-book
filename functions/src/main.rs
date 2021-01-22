1e9d92e0b4c3aa5946ac4078eb9a7a613a5686f8
Christopher S Martin <shady@shadyproject.net>
2021-01-21 22:50:04 -0800

    function stuff

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,9 +2,22 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 6);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    println!("the value after calling the function is: {}", plus_one(x));
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
