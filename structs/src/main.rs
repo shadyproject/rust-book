cb2716de48fee9665a6bcb8d22778a8b3bdd66e8
Christopher S Martin <shady@shadyproject.net>
2021-01-21 23:45:26 -0800

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,26 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("test@test.com"),
+        username: String::from("test_user"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{}", user1.username);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
